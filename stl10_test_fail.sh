ba94eb0543c72a66d83a0660b7069a3a0ff5c790
HexagonPrime <805335677@qq.com>
2019-12-02 18:13:41 +0000

    script

--- stl10_test_fail.sh
+++ stl10_test_fail.sh
@@ -2,7 +2,7 @@
 #SBATCH --output=/mnt/lustre/users/%u/%j.out
 #SBATCH --job-name=gpu
 #SBATCH --gres=gpu
-#SBATCH --mem=25000
+"#SBATCH --mem=25000"
 "#SBATCH --constrain=v100"
 
 conda activate iic
