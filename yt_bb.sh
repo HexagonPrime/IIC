ff652da47665bbdc000cd42a3534ae8cd2a031fb
HexagonPrime <805335677@qq.com>
2020-02-16 21:46:37 +0000

    YT_BB wrapping up

--- yt_bb.sh
+++ yt_bb.sh
@@ -8,4 +8,4 @@
 conda activate py2
 #module load libs/cuda
 
-export CUDA_VISIBLE_DEVICES=1 && python -m code.scripts.cluster.YT_BB_script --model_ind 640  --arch ClusterNet5gTwoHead --mode IID --dataset YT_BB --dataset_root /users/k1763920/yt_bb_util --gt_k 10 --output_k_A 70 --output_k_B 10 --lamb 1.0 --lr 0.0001  --num_epochs 4 --batch_sz 660 --num_dataloaders 3 --num_sub_heads 5 --crop_orig --rand_crop_sz 20 --input_sz 32 --head_A_first --head_B_epochs 2
+export CUDA_VISIBLE_DEVICES=0 && python -m code.scripts.cluster.YT_BB_script --model_ind 640  --arch ClusterNet5gTwoHead --mode IID --dataset YT_BB --dataset_root /users/k1763920/yt_bb_util --gt_k 10 --output_k_A 70 --output_k_B 10 --lamb 1.0 --lr 0.0001  --num_epochs 4 --batch_sz 660 --num_dataloaders 3 --num_sub_heads 5 --crop_orig --rand_crop_sz 20 --input_sz 32 --head_A_first --head_B_epochs 2
