96055ad3a666ac6bbcd1aeba9bb3ce2f3943fd97
HexagonPrime <805335677@qq.com>
2020-02-27 16:54:12 +0000

    change yt_bb circular back to choose the last frame

--- code/datasets/clustering/YT_BB.py
+++ code/datasets/clustering/YT_BB.py
@@ -32,10 +32,10 @@ class YT_BB(Dataset):
             # Circular if this frame required exceeds frames the segment has.
             # this_frame = frame % len(group)
             # Choose the last frame if the required frame does not exist
-            if this_frame > len(this_group)-1:
-                this_frame = len(this_group)-1
-            print 'Frame: ' + str(this_frame)
-            this_row = group.iloc[[this_frame]]
+            if frame > len(this_group)-1:
+                frame = len(this_group)-1
+            print 'Frame: ' + str(frame)
+            this_row = group.iloc[[frame]]
             this_class = this_row['class_id'].iat[0]
             if included[this_class] < 1000:
                 self.dataset.append(this_row)
