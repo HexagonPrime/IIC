67b7b2eb84ec0d225d65d5fa288bbaac5a666544
HexagonPrime <805335677@qq.com>
2020-03-27 23:38:25 +0000

    change mapping loader, output train and test stats seperately

--- code/utils/cluster/YT_BB_data.py
+++ code/utils/cluster/YT_BB_data.py
@@ -25,7 +25,7 @@ def cluster_twohead_create_YT_BB_dataloaders(config):
     config.train_partitions_head_A = config.train_partition
     config.train_partitions_head_B = config.train_partitions_head_A
 
-    config.mapping_assignment_partitions = config.test_partition
+    config.mapping_assignment_partitions = config.train_partition
     config.mapping_test_partitions = config.test_partition
 
     dataset_class = YT_BB  #TODO YT_BB custom class
